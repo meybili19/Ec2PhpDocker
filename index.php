9bafc07fbaac8cfbcdf70e96018afecdc6567069
Meybili <meybili200219@gmail.com>
2024-12-04 17:14:05 -0500

    feat: :fire: Update

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 </head>
 <body>
 	<p> Mi nombre es Meybili Olivares</p>
-    <h1>IP Pública de la instancia EC2: <?php echo getenv('EC2_PUBLIC_IP'); ?></h1>
-
+	<h1><?php echo gethostname();?></h1>
 </body>
 </html>
